b0670e1d42993abe5936a6f741163409e14fdfec
loganrun <bigdog_41@hotmai.com>
2024-02-02 07:44:28 -0800

    small updates

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let navBod = document.getElementById('title')
 navBod.innerHTML = '<h1>Great African Americans in Tech</h1>'
 navBod.classList.add('flex-ctr')
 navBod.style.color = 'white'
+
+let hannon = document.getElementById('hero#1')
+hannon.innerHTML = '<h1>Software Enginer</h1>'
+hannon.innerHTML = '<h1 style="font-size: 78px">?</h1>'
