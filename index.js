17e453fcd7c07e8a7e37ca98f7f9590dc494f478
loganrun <bigdog_41@hotmai.com>
2024-02-02 16:59:09 -0800

    add persons

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ navBod.style.color = 'white'
 let hannon = document.getElementById('hero#1')
 hannon.innerHTML = '<h1>Software Enginer</h1>'
 hannon.innerHTML = '<h1 style="font-size: 78px">?</h1>'
+
+const innovators = [{firstName: 'Marc', 
+                        lastName: 'Hannah',
+                        image: 'images/MarcHannah.jpg',
+                        occupation: 'Computer Graphics Designer',
+                        question: 'This person was one of the 7 founders of Silicon Graphics Incorporated (SGI)',
+                        info: 'Marc headed the team that created the Geometry Engine- the predecesor of modern day graphics cards. Marc and his team created the 3-D graphics chip for the Nintendo 64 and for movies such as Jurassic Park and Terminator 2'
+                        },
+                        {firstName: 'Joshua',
+                        lastName: 'Hernandez',
+                        image: 'images/MarcHannah.jpg',
+                        occupation: 'Computer Graphics Designer',
+                        question: 'This person was one of the 7 founders of Silicon Graphics Incorporated (SGI)',
+                        info: 'Joshua is a computer graphics engineer at SGI. Joshua is the founder of the Silicon Graphics Incorporated (SGI) and the founder of the Silicon Graphics Group (SGG). Joshua is the founder of the Silicon Graphics Incorporated (SGI) and the founder of the Silicon Graphics Group (SGG). Joshua is the founder of the Silicon Graphics Incorporated'
+                        }
+                        ]
