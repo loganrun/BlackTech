b6a0100e7685e93adca56998ff27e2a0d68c77b0
loganrun <bigdog_41@hotmai.com>
2024-02-04 18:06:48 -0800

    Jesse

--- index.js
+++ index.js
@@ -6,24 +6,6 @@ navBod.style.color = 'white'
 let mark = document.getElementById('intro1')
 mark.innerHTML = '<h2>This person invented the Personal Computer</h2>'
 mark.style.color = 'white'
-
-let marion = document.getElementById('intro2')
-marion.innerHTML = '<h2>This person invented Voice Over Internet Protocol</h2>'
-marion.style.color = 'white'
-
-let glayds = document.getElementById('intro3')
-glayds.innerHTML = `<h2>This person's work is the foundation of GPS</h2>`
-glayds.style.color = 'white'
-
-let jesse = document.getElementById('intro4')
-jesse.innerHTML = `<h2>This person holds the patent for the mobile data</br>
- phone and the wireless base station</h2>`
-jesse.style.color = 'white'
-
-let hannon = document.getElementById('intro5')
-hannon.innerHTML = `<h2>This person invented the 3-D graphics chip </h2>`
-hannon.style.color = "white"
-
 let bMark = document.getElementById("mark1")
 let markDiv = document.getElementById('Mark');
 let overlayMark = markDiv.querySelector('.overlay');
@@ -46,6 +28,9 @@ bMark.addEventListener("click", (e)=>{
     e.preventDefault()
 })
 
+let marion = document.getElementById('intro2')
+marion.innerHTML = '<h2>This person invented Voice Over Internet Protocol</h2>'
+marion.style.color = 'white'
 let cMarion = document.getElementById("marion1");
 let marionDiv = document.getElementById('Marion');
 let overlayMarion = marionDiv.querySelector('.overlay');
@@ -67,6 +52,64 @@ cMarion.addEventListener("click", (e)=>{
     cMarion.appendChild(info);
     e.preventDefault()
 })
+
+let glayds = document.getElementById('intro3')
+glayds.innerHTML = `<h2>This person's work is the foundation of GPS</h2>`
+glayds.style.color = 'white'
+let wGlayds = document.getElementById("glayds1");
+let glaydsDiv = document.getElementById('Gladys');
+let overlayGladys = glaydsDiv.querySelector('.overlay');
+let but3 =document.getElementById('but3');
+wGlayds.addEventListener("click", (e)=>{
+    overlayGladys.remove();
+    but3.remove();
+    let title = document.createElement('h1'); 
+    title.textContent = 'Dr. Glayds West';
+    let info = document.getElementById('glayds1'); 
+    info = document.createElement('div');
+    info.innerHTML=`<p>Gladys Mae West is an American mathematician known for her <br>contributions to the mathematical modeling of the shape of the Earth,</br> and her work on the development of the satellite geodesy models that</br> were eventually incorporated into the Global Positioning System (GPS).</br> West was inducted into the United States Air Force Hall of Fame in 2018. </p>`;
+    title.style.color = 'white';
+    title.classList.add('flex-ctr');
+    info.style.color = 'white';
+    info.classList.add('flex-ctr');
+    wGlayds.appendChild(title);
+    wGlayds.appendChild(info);
+    e.preventDefault()
+})
+
+let jesse = document.getElementById('intro4')
+jesse.innerHTML = `<h2>This person holds the patent for the mobile data</br>
+phone and the wireless base station</h2>`
+jesse.style.color = 'white'
+
+let rJesse = document.getElementById("jesse1");
+let jesseDiv = document.getElementById('Jesse');
+let overlayJesse = jesseDiv.querySelector('.overlay');
+let but4 =document.getElementById('but4');
+
+rJesse.addEventListener("click", (e)=>{
+    overlayJesse.remove();
+    but4.remove();
+    let title = document.createElement('h1'); 
+    title.textContent = 'Jesse Russel';
+    let info = document.getElementById('jesse1'); 
+    info = document.createElement('div');
+    info.innerHTML=`<p>Russell is known for his invention of the digital cellular base station and the </br> fibre optic microcell utilizing high power linear amplifier technology</br> and digital modulation techniques, which enabled new digital services for</br> cellular mobile users. </p>`;
+    title.style.color = 'white';
+    title.classList.add('flex-ctr');
+    info.style.color = 'white';
+    info.classList.add('flex-ctr');
+    rJesse.appendChild(title);
+    rJesse.appendChild(info);
+    e.preventDefault()
+})
+let hannon = document.getElementById('intro5')
+hannon.innerHTML = `<h2>This person invented the 3-D graphics chip </h2>`
+hannon.style.color = "white"
+
+
+
+
 
 
 
